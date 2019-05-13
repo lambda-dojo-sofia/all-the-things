2340bf07e7b8990c78a26799fed0ed388b73a844
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-05-13 20:45:28 +0300

    some loops

--- 2019-05-13/inverted/lib/inverted.ex
+++ 2019-05-13/inverted/lib/inverted.ex
@@ -1,11 +1,24 @@
 defmodule Inverted do
+
   def load_comic_title (n) do
     {:ok, response} = SimpleHttp.get "https://xkcd.com/#{n}/info.0.json"
-    %{"title" => title} = Jason.decode!(response.body)
-    title
+    if response.status == 200 do
+      %{"title" => title} = Jason.decode!(response.body)
+      title
+    end
+  end
+
+  def collect_all_titles(titles, current_title) do
+    if current_title > 25 do
+      titles
+    else
+      n = load_comic_title current_title
+      [n | (collect_all_titles titles, current_title + 1)]
+    end
   end
 
   def hello do
-    load_comic_title 2
+    collect_all_titles [], 1
+    #    load_comic_title 3000
   end
 end
