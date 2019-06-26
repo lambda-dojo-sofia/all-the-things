efdfafd7c1e1395f3d0566b562d7453d4b7e7b6e
Jordan Biserkov <j.valkov-biserkov@klarna.com>
2019-06-26 21:20:20 +0300

    Pomodoro 2

--- 2019-06-26/clojure/src/dojo/core.clj
+++ 2019-06-26/clojure/src/dojo/core.clj
@@ -1,32 +1,40 @@
 (ns dojo.core
+
   (:gen-class))
 
-(require '[clojure.set :as set])
+(require '[clojure.set])
 
-(slurp )
+
 
 ;; leg -> log -> cog
 
 (def start "leg")
 (def end "cog")
-(def dict {})
+(def dict (into #{} (line-seq (clojure.java.io/reader "dictionary.txt"))))
 
 (defn same-len [start]
   (fn [word] (= (count start) (count word))))
 
 (def input-dict (filter (same-len start) dict))
 
-((off-by-one "leg") "log")
-((off-by-one "leg") "cog")
-(defn off-by-one [current]
+
+(defn off-by-one? [current]
   (fn [word]
     (= 1 (count (filter false? (map = word current))))))
 
-(defn neigbours [word chain]
-  (set/difference (into #{} (filter off-by-one input-dict))
-                  chain))
+(defn neighbours [word chain]
+  (clojure.set/difference (into #{} (filter (off-by-one? word) input-dict))
+                  (set chain)))
 
-
+(defn word-chain [start end]
+  (loop [chain [start]
+         current-word start
+         words (neighbours current-word chain)]
+    (if (any #(contains? % end ) words )
+      (concat chain end)
+      (recur
+       (map (fn [word]
+             (neighbours word (concat chain current-word))) words)))))
 
 (defn -main
   ""
