a6a723311bb6f05c96b80ba5f0e49f5d3c435a6b
ignorabilis <irina.yaroslavova@ignorabilis.com>
2019-10-07 20:24:31 +0300

    digest

--- 2019-10-07/better-than-best/src/better_than_best/core.clj
+++ 2019-10-07/better-than-best/src/better_than_best/core.clj
@@ -1,6 +1,18 @@
-(ns better-than-best.core)
+(ns better-than-best.core
+    (:import (org.kocakosm.jblake2 Blake2b)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(def p "pooppoop")
+
+(defn string->hash [str]
+      (.digest
+        (.update
+          (Blake2b. (int 5))
+          (bytes
+            (byte-array
+              (map (comp byte int)
+                   str))))))
