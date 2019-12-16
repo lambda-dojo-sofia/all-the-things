9ec177e4beec49a25d9d6e8be661986bb1c9574c
Boyan Bonev <b.bonev@redbuffstudio.com>
2019-12-16 21:09:15 +0200

    Poopy poop.

--- 2019-12-16/rust/poop.rs
+++ 2019-12-16/rust/poop.rs
@@ -9,14 +9,37 @@ fn at_least_two(n: u32, current_digit: u32) -> bool {
     return current_digit == last_digit || at_least_two(n / 10, last_digit)
 }
 
+fn exactly_two(n: u32, current_digit: u32, count: &mut u32) -> bool {
+
+    if n < 10 {
+        if current_digit == n {
+            *count += 1;
+        }
+        return *count == 2;
+    }
+
+    let last_digit = n % 10;
+    if current_digit != last_digit && *count == 2 {
+        return true;
+    }
+
+    if current_digit == last_digit {
+        *count += 1;
+    } else {
+        *count = 1;
+    }
+    return exactly_two(n / 10, last_digit, count);
+}
+
+
 fn decreasing(n: u32, current_digit: u32) -> bool {
     if n < 10 {
         return current_digit >= n
     }
-    let last_digit = n % 10;
-    return current_digit >= last_digit || decreasing(n / 10, last_digit)
-}
 
+    let last_digit = n % 10;
+    return current_digit >= last_digit && decreasing(n / 10, last_digit)
+}
 
 fn main() {
     // Statements here are executed when the compiled binary is called
@@ -27,6 +50,11 @@ fn main() {
     let end :u32 = range[1].parse().unwrap();
 
     let whole_range = start..end;
+    let whole_range2 = start..end;
 
-    println!("Valid numbers: {}", whole_range.filter(|n| at_least_two(*n, 11) && decreasing(*n, 0)).collect::<Vec<u32>>().len());
+    let mut real_count: u32 = 1;
+    let count: &mut u32 = &mut real_count;
+
+    println!("Valid numbers 1: {}", whole_range.filter(|n| at_least_two(*n, 11) && decreasing(*n, 10)).collect::<Vec<u32>>().len());
+    println!("Valid numbers 2: {}", whole_range2.filter(|n| exactly_two(*n / 10, *n % 10, count) && decreasing(*n, 10)).collect::<Vec<u32>>().len());
 }
