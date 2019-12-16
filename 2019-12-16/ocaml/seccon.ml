64838805582f4405247cf566587d65d96c90d7a4
Albert Peschar <albert@peschar.net>
2019-12-16 20:52:24 +0200

    Done

--- 2019-12-16/ocaml/seccon.ml
+++ 2019-12-16/ocaml/seccon.ml
@@ -23,11 +23,29 @@ let valid n =
     && two_adj_digits n
     && increasing_digits n
 
-let rec count_valid' acc lo hi =
+let rec count_valid' acc valid lo hi =
     if lo > hi then acc else
-    count_valid' (if valid lo then acc + 1 else acc) (lo+1) hi
+    count_valid' (if valid lo then acc + 1 else acc) valid (lo+1) hi
 
 let count_valid = count_valid' 0
 
-let () = Format.printf "%d\n"
-    (count_valid (int_of_string Sys.argv.(1)) (int_of_string Sys.argv.(2)))
+let rec count_adj' acc n =
+    if n < 10 then acc else
+    if last n = last_but_one n then count_adj' (acc+1) (chop_last n)
+    else acc
+
+let count_adj = count_adj' 1
+
+let rec chop_last_n c n =
+    if c < 1 then n
+    else chop_last_n (c-1) (chop_last n)
+
+let rec exactly_two_adj_digits n =
+    if n < 10 then false else
+    if count_adj n = 2 then true
+    else exactly_two_adj_digits (chop_last_n (count_adj n) n)
+
+let valid2 n = valid n && exactly_two_adj_digits n
+
+let run valid = Format.printf "%d\n"
+    (count_valid valid (int_of_string Sys.argv.(1)) (int_of_string Sys.argv.(2)))
